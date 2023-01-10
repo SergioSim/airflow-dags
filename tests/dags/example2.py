1c79afba7a5c78abd0bbada7914e8efe94316de9
SergioSim <Sergio.Simonian@etu.unice.fr>
2023-01-10 13:12:26 +0100

    rename example dag

--- tests/dags/example2.py
+++ tests/dags/example2.py
@@ -38,7 +38,7 @@ from airflow.operators.bash import BashOperator
 
 # [START instantiate_dag]
 with DAG(
-    "tutorial",
+    "tutorial2",
     # [START default_args]
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
